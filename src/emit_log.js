57a254b98e52543f0c3b8623aaac14e129af6c43
quytm <minhquylt95@gmail.com>
2018-09-21 11:00:59 +0700

    pub/sub in RabbitMQ

--- src/emit_log.js
+++ src/emit_log.js
@@ -7,10 +7,15 @@ amqp.connect('amqp://localhost', function(err, conn) {
     var ex = 'logs';
     var msg = process.argv.slice(2).join(' ') || 'Hello World!';
 
+    // Instead of `assertQueue`, we use `assertExchange` for adding message into exchange
+    // `fanout` is a one type of exchange
     ch.assertExchange(ex, 'fanout', {durable: false});
+    // Instead of `sendToQueue`, use `publish`
+    // empty value mean that we don't want to send the message to any specific queue
     ch.publish(ex, '', new Buffer(msg));
     console.log(" [x] Sent %s", msg);
   });
 
   setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
+
--- src/receive_logs.js
+++ src/receive_logs.js
@@ -3,18 +3,26 @@
 var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
+
   conn.createChannel(function(err, ch) {
     var ex = 'logs';
 
     ch.assertExchange(ex, 'fanout', {durable: false});
 
+    // Temporary queues
+    // exclusive flag: when the connection that declared it closes, queue is deleted
     ch.assertQueue('', {exclusive: true}, function(err, q) {
+      // Random queue name is generated, can get it by `q.queue`
       console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+      // Notifying to exchange send message to queue by `bindQueue` method
       ch.bindQueue(q.queue, ex, '');
 
       ch.consume(q.queue, function(msg) {
         console.log(" [x] %s", msg.content.toString());
       }, {noAck: true});
     });
+    
   });
+
 });
+
